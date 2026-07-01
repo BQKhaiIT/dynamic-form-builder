68882bb3f0a95ef40a637af68e81434e2b6c27a7
BQKhaiIT <nobiemon15062004@gmail.com>
2026-07-01 11:27:10 +0700

    feat: add database schema and Eloquent models

---
+++ database/migrations/2026_07_01_042415_create_form_fields_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form_fields', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('form_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('label');
+
+            $table->enum('type', [
+                'text',
+                'number',
+                'date',
+                'color',
+                'select'
+            ]);
+
+            $table->string('placeholder')->nullable();
+
+            $table->boolean('required')->default(false);
+
+            $table->unsignedInteger('sort_order')->default(0);
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form_fields');
+    }
+};
